f61152927d6f1a1aaec79c0f599e86099d3e5f6e
Pair <pair@pivotal.io>
2016-11-23 12:14:14 +0800

    New constants file.

--- Tsundoku/Constants.swift
+++ Tsundoku/Constants.swift
@@ -1,9 +1,3 @@
 import Foundation
 
-struct Constants {
-    struct Credentials {
-        static let awsAccessKeyId = "XXX"
-        static let associateTag = "XXX"
-        static let awsSecretKey = "XXX"
-    }
-}
+struct Constants {}
